7116fbbd2c880e96e16bf84f7c8f045d8377d37f
AndrewKooklin <bolt999@yandex.ru>
2024-04-05 13:16:36 +0300

    Add Management folder

---
+++ MyFlatWEB/Areas/Management/ManagementHostingStartup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(MyFlatWEB.Areas.Management.ManagementHostingStartup))]
+namespace MyFlatWEB.Areas.Management
+{
+    public class ManagementHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
