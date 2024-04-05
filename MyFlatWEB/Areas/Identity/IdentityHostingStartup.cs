fcb1c696517ec6cdcc000a360872a67896e388c5
AndrewKooklin <bolt999@yandex.ru>
2024-04-05 10:12:39 +0300

    Add new project

---
+++ MyFlatWEB/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyFlatWEB.Data;
+
+[assembly: HostingStartup(typeof(MyFlatWEB.Areas.Identity.IdentityHostingStartup))]
+namespace MyFlatWEB.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
