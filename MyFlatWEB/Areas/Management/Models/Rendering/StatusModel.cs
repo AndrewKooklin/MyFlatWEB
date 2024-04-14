6ae3be032d9f6a06c11086a1f72f0e97e71ad671
AndrewKooklin <bolt999@yandex.ru>
2024-04-14 13:01:53 +0300

    Add StatusModel, ServiceModel

---
+++ MyFlatWEB/Areas/Management/Models/Rendering/StatusModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFlatWEB.Areas.Management.Models.Rendering
+{
+    public class StatusModel : BaseModel
+    {
+        [Required]
+        [MinLength(3)]
+        public string StatusName { get; set; }
+    }
+}
