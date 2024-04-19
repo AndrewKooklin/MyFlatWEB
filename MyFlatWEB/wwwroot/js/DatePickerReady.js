7db0174de2221a835be5995c92152a60ecb04221
AndrewKooklin <bolt999@yandex.ru>
2024-04-19 16:30:43 +0300

    Add _InputDataPartial

---
+++ MyFlatWEB/wwwroot/js/DatePickerReady.js
@@ -0,0 +1,12 @@
+﻿ //$(function() {
+ //       $("#date1").datepicker({
+ //           dateFormat: 'dd-mm-yy',
+ //           defaultDate: "24-09-2019"
+ //       });
+ //});
+$(function () {
+    "use strict";
+    jQuery('#date1').datepicker({
+        dateFormat: 'dd.mm.yyyy'
+    });
+});
