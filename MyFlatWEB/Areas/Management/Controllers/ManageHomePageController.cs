88f16dc587f5b3ca83bdd9e0ca5268b9083a6a97
AndrewKooklin <bolt999@yandex.ru>
2024-04-23 16:55:59 +0300

    Add EditHopePage

---
+++ MyFlatWEB/Areas/Management/Controllers/ManageHomePageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFlatWEB.Areas.Management.Controllers
+{
+    [Area("Management")]
+    [Route("Management/ManageHomePage/")]
+    public class ManageHomePageController : Controller
+    {
+
+
+
+        [Route("HomePage")]
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
