dd47334051778001383f1b60bbc952305688e05c
AndrewKooklin <bolt999@yandex.ru>
2024-06-21 20:04:05 +0300

    Add CheckUserToDB

--- MyFlatWEB/Data/Repositories/Abstract/IAccountRepository.cs
+++ MyFlatWEB/Data/Repositories/Abstract/IAccountRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,32 @@ namespace MyFlatWEB.Data.Repositories.Abstract
 {
     public interface IAccountRepository
     {
+        Task<bool> CheckUserToDB(LoginModel model);
 
+        void LogoutUser();
+
+        Task<bool> CreateUser(RegisterModel model);
+
+        List<string> GetRoleNames();
+
+        IEnumerable<IdentityRole> GetRoles();
+
+        Task<List<string>> GetUserRoles(LoginModel model);
+
+        Task<bool> CreateRole(IdentityRole role);
+
+        Task<bool> DeleteRole(string id);
+
+        List<IdentityUser> GetUsers();
+
+        UserWithRolesModel GetUserWithRoles(string id);
+
+        Task<bool> AddRoleToUser(RoleUserModel model);
+
+        Task<bool> DeleteRoleUser(RoleUserModel model);
+
+        Task<bool> DeleteRolesUser(string id);
+
+        Task<bool> DeleteUser(string id);
     }
 }
